605392e73837ec6fb7dd930543a33cb3dc0ec685
eric2969 <eric2969@gmail.com>
2024-06-02 00:26:26 +0800

    book_crt_demo

--- book_crt.js
+++ book_crt.js
@@ -1,18 +1,43 @@
 $(document).ready(function(){
     $("#book_crt").click(function(){
+        var f_date = $("#f_date").val();
+        var f_time = $("#f_time").val();
+        var f_name = $("#f_name").val();
+        var f_phone = $("#f_phone").val();
+        var f_people = $("#f_people").val();
+        var f_other = $("#f_other").val();
         $.ajax({
             type: "POST",
 			url: "http://localhost/book_crt.php",
 			datatype: "json",
 			data:{
-				
+				date: f_date,
+                time: f_time,
+                name: f_name,
+                phone: f_phone,
+                people: f_people,
+                other: f_other,
 			},
 			contentType: "application/x-www-form-urlencoded;charset=UTF-8",
 			success: function(data) {
-                
+                console.log(data);
+                $("f_main").css('display','none');
+                if(data == "successful"){
+                    console.log("ok");
+                    $("#success").css('display','block');
+                }
+                else if(data == "error"){
+                    console.log("error");
+                    $("#fail").css('display','block');
+                }
+                else{
+                    console.log(data);
+                }
             },
             error: function(jqXHR) {
-               
+                $("f_main").css('display','none');
+                $("#fail").css('display','block');
+                alert("error" + jqXHR.status + "\n");
             }
         })
     })
--- book_crt.php
+++ book_crt.php
@@ -1,2 +1,26 @@
 <?php
+    $db_host = "localhost";
+    $db_user = "iim";
+    $db_pass = "fuckyou";
+    $db_name = "iim";
+    $con = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+    $con -> query("SET NAMES 'utf8'");
+
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $people = $_POST['people'];
+    $other = $_POST['other'];
+
+    $query = "INSERT INTO book (`date`, `time`, `name`, `phone`, `people`, `other`) VALUES (?,?,?,?,?,?)";
+    $main = $con -> prepare($query);
+    $main -> bind_param("ssssss", $date, $time, $name, $phone, $people, $other);
+    $result = $main -> execute();
+    if($result){
+        echo "successful";
+    }
+    else{
+        echo "error";
+    }
 ?>
