c2f0d76a82b3c6d6dc5e6b78b368b2881dae7e27
eric2969 <eric2969@gmail.com>
2024-06-02 05:09:31 +0800

    finish_acc&query_price

--- backend/record_crt.php
+++ backend/record_crt.php
@@ -1,2 +1,25 @@
 <?php
+    $db_host = "localhost";
+    $db_user = "iim";
+    $db_pass = "fuckyou";
+    $db_name = "iim";
+    $con = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+    $con -> query("SET NAMES 'utf8'");
+
+    $date = $_POST['date'];
+    $month = $_POST['month'];
+    $week = $_POST['week'];
+    $time = $_POST['time'];
+    $price = $_POST['price'];
+
+    $query = "INSERT INTO money (`date` , `month`, `week`, `time`, `price`) VALUES (?,?,?,?,?)";
+    $main = $con -> prepare($query);
+    $main -> bind_param("sssss", $date, $month, $week, $time, $price);
+    $result = $main -> execute();
+    if($result){
+        echo "successful";
+    }
+    else{
+        echo "error";
+    }
 ?>
