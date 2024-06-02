68975d2a0f67cf56c3f63dab67b3a12ccb0ad9ce
eric2969 <eric2969@gmail.com>
2024-06-02 22:17:21 +0800

    modify_remember_time

--- backend/admin_login.php
+++ backend/admin_login.php
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($input['username']) && isset($
     if ($stmt->num_rows > 0 && password_verify($password, $hashed_password)) {
         $_SESSION['admin'] = $username;
         if ($remember) {
-            setcookie('admin', $username, time() + (86400 * 30), "/"); // 30天
+            setcookie('admin', $username, time() + (300), "/"); // 5min
         }
         echo json_encode(['success' => true, 'message' => '登入成功']);
     } else {
