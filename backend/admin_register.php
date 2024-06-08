93c3714d10d87cbed8f8a04d863a5105d3311da6
eric2969 <eric2969@gmail.com>
2024-06-08 17:06:19 +0800

    chk_user_exist

--- backend/admin_register.php
+++ backend/admin_register.php
@@ -14,19 +14,30 @@ $input = json_decode(file_get_contents('php://input'), true);
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($input['username']) && isset($input['password'])) {
     $username = $input['username'];
     $password = password_hash($input['password'], PASSWORD_BCRYPT);
-
-    $query = "INSERT INTO admins (`username`, `password`) VALUES (?, ?)";
+    //find exist user
+    $query = "SELECT `indice` FROM admins WHERE `username` = ?";
     $stmt = $con->prepare($query);
-    $stmt->bind_param("ss", $username, $password);
-
-    if ($stmt->execute()) {
-        echo json_encode(['success' => true, 'message' => '註冊成功']);
-    } else {
-        echo json_encode(['success' => false, 'message' => '註冊失敗']);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($hashed_password);
+    $stmt->fetch();
+    if($stmt->num_rows > 0){
+        echo json_encode(['success' => false, 'message' => '使用者已被註冊!']);
     }
-    $stmt->close();
+    else{
+        $query = "INSERT INTO admins (`username`, `password`) VALUES (?, ?)";
+        $stmt = $con->prepare($query);
+        $stmt->bind_param("ss", $username, $password);
+        if ($stmt->execute()) {
+            echo json_encode(['success' => true, 'message' => '註冊成功!']);
+        } else {
+            echo json_encode(['success' => false, 'message' => '註冊失敗!']);
+        }
+        $stmt->close();
+    }
 } else {
-    echo json_encode(['success' => false, 'message' => '無效的請求']);
+    echo json_encode(['success' => false, 'message' => '無效的請求!']);
 }
 
 $con->close();
